b7178cbc6396d2ab8020cfefbf58ba257d3ee3f5
Christopher Coverdale <chris.coverdale24@gmail.com>
2020-11-21 14:14:23 +0000

    update docs for job_negotiation protocol

--- src/job_negotiation/mod.rs
+++ src/job_negotiation/mod.rs
@@ -1,2 +1,11 @@
+//! The sub protocol allows a Job Negotiator to intermediate between Upstream
+//! Nodes (Mining Pools), Bitcoind and Proxies/Mining Devices. The protocol
+//! allows a block template to be negotiated with a mining pool, including the
+//! transaction set.
+//!
+//! The results of the job negotiation with a Mining Pool means downstream
+//! nodes (Mining Farms/Devices) can use the same negotiation result on all
+//! their connections.
+
 /// Job Negotiation subprotocol messages.
 pub mod messages;
