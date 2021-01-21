819a03fc66373d49ba54c0f5733520e1ad90827b
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-01-21 13:41:16 +0000

    impl deserialize flags for BitFlag in job_negotiation

--- src/job_negotiation/flags.rs
+++ src/job_negotiation/flags.rs
@@ -2,6 +2,7 @@ use crate::common::{BitFlag, Protocol, ToProtocol};
 
 /// Feature flags that can be passed to a SetupConnection message for the
 /// job negotiation protocol. Each flag corresponds to a set bit.
+#[derive(Debug, PartialEq, Clone)]
 pub enum SetupConnectionFlags {
     // TODO: Add hyperlinks to all everything between ``
     /// Flag indicating that the `mining_job_token` from `AllocateMiningJobToken.Success`
@@ -26,6 +27,27 @@ impl BitFlag for SetupConnectionFlags {
         match self {
             SetupConnectionFlags::RequiresAsyncJobMining => 1,
         }
+    }
+
+    /// Gets a vector of flag enum representations from a u32.
+    ///
+    /// Example:
+    ///
+    /// ```rust
+    /// use stratumv2::job_negotiation::SetupConnectionFlags;
+    /// use stratumv2::common::BitFlag;
+    ///
+    /// let flags = SetupConnectionFlags::deserialize_flags(1);
+    /// assert_eq!(flags[0], SetupConnectionFlags::RequiresAsyncJobMining);
+    /// ```
+    fn deserialize_flags(flags: u32) -> Vec<SetupConnectionFlags> {
+        let mut der_flags = Vec::new();
+
+        if flags & SetupConnectionFlags::RequiresAsyncJobMining.as_bit_flag() != 0 {
+            der_flags.push(SetupConnectionFlags::RequiresAsyncJobMining)
+        }
+
+        der_flags
     }
 }
 
