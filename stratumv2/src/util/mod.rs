193214d300aca274c8d8069cf92a4fde101609dc
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-02-21 14:41:50 +0000

    add system_unix_time_u32 to utils module

--- stratumv2/src/util/mod.rs
+++ stratumv2/src/util/mod.rs
@@ -1,2 +1,12 @@
+use crate::Result;
+use std::time::SystemTime;
+
 mod channel_id;
 pub use channel_id::new_channel_id;
+
+/// Convert SystemTime to a Unix timestamp as a u32.
+pub fn system_unix_time_to_u32(time: &SystemTime) -> Result<u32> {
+    Ok(time
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .map(|duration| duration.as_secs() as u32)?)
+}
