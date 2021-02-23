5b6e416b8d0c8ed51e65c62826a523a65403f8f9
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-02-23 18:48:39 +0000

    add helper functions to convert bytes little endian to u32 and u16

--- stratumv2/src/util/mod.rs
+++ stratumv2/src/util/mod.rs
@@ -10,3 +10,13 @@ pub fn system_unix_time_to_u32(time: &SystemTime) -> Result<u32> {
         .duration_since(SystemTime::UNIX_EPOCH)
         .map(|duration| duration.as_secs() as u32)?)
 }
+
+/// Convert a byte array in little endian format to a u32.
+pub fn le_bytes_to_u32(bytes: [u8; 4]) -> u32 {
+    (bytes[3] as u32) << 24 | (bytes[2] as u32) << 16 | (bytes[1] as u32) << 8 | (bytes[0] as u32)
+}
+
+/// Convert a byte array in little endian format to a u16.
+pub fn le_bytes_to_u16(bytes: [u8; 2]) -> u16 {
+    (bytes[1] as u16) << 8 | (bytes[0] as u16)
+}
