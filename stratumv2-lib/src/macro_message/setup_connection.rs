f41ea165c4d18ee3b4faac228c14bea0d4e78c17
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-04-08 14:47:34 +0100

    add Protocol Enum to SetupConnection and add tests

--- stratumv2-lib/src/macro_message/setup_connection.rs
+++ stratumv2-lib/src/macro_message/setup_connection.rs
@@ -1,4 +1,5 @@
 pub mod macro_prelude {
+    pub use crate::common::Protocol;
     pub use crate::error::{Error, Result};
     pub use crate::frame::Frameable;
     pub use crate::parse::{ByteParser, Deserializable, Serializable};
@@ -11,7 +12,7 @@ pub mod macro_prelude {
 /// sub protocol.
 #[macro_export]
 macro_rules! impl_setup_connection {
-    ($flags_type:ident) => {
+    ($protocol:path, $flags_type:ident) => {
         use crate::macro_message::setup_connection::macro_prelude::*;
 
         /// SetupConnection is the first message sent by a client on a new connection.
@@ -43,8 +44,7 @@ macro_rules! impl_setup_connection {
         ///
         /// let job_negotiation_connection = job_negotiation::SetupConnection::new(
         ///    2,
-        ///    2,
-        ///    job_negotiation::SetupConnectionFlags::REQUIRES_ASYNC_JOB_MINING,
+        ///    2, job_negotiation::SetupConnectionFlags::REQUIRES_ASYNC_JOB_MINING,
         ///    "0.0.0.0",
         ///    8545,
         ///    "Bitmain",
@@ -56,6 +56,9 @@ macro_rules! impl_setup_connection {
         /// ```
         #[derive(Debug, Clone)]
         pub struct SetupConnection {
+            /// Used to indicate the protocol the client wants to use on the new connection.
+            pub(crate) protocol: Protocol,
+
             /// The minimum protocol version the client supports. (current default: 2)
             pub min_version: u16,
 
@@ -122,6 +125,7 @@ macro_rules! impl_setup_connection {
                 }
 
                 Ok(SetupConnection {
+                    protocol: $protocol,
                     min_version,
                     max_version,
                     flags,
@@ -140,6 +144,7 @@ macro_rules! impl_setup_connection {
         impl Serializable for SetupConnection {
             fn serialize<W: io::Write>(&self, writer: &mut W) -> Result<usize> {
                 Ok([
+                    self.protocol.serialize(writer)?,
                     self.min_version.serialize(writer)?,
                     self.max_version.serialize(writer)?,
                     self.flags.serialize(writer)?,
@@ -157,6 +162,7 @@ macro_rules! impl_setup_connection {
 
         impl Deserializable for SetupConnection {
             fn deserialize(parser: &mut ByteParser) -> Result<SetupConnection> {
+                let _protocol = Protocol::deserialize(parser)?;
                 let min_version = u16::deserialize(parser)?;
                 let max_version = u16::deserialize(parser)?;
                 let flags = $flags_type::deserialize(parser)?;
@@ -187,4 +193,179 @@ macro_rules! impl_setup_connection {
             }
         }
     };
+}
+
+#[cfg(test)]
+macro_rules! impl_setup_connection_tests {
+    ($type:ident, $empty_flag:expr, $flags:expr) => {
+        use crate::common::Protocol;
+        use crate::error::Result;
+        use crate::frame::frame;
+        use crate::parse::{deserialize, serialize};
+        use crate::types::U24;
+        use std::collections::HashMap;
+        use std::convert::TryFrom;
+
+        // Helper test function to generate a default SetupConnection message
+        // with optional args passed through a HashpMap.
+        fn default_setup_connection(args: HashMap<String, String>) -> Result<$type> {
+            let mut min_version = 2;
+            let mut max_version = 2;
+            let mut vendor = "Bitmain";
+            let mut firmware = "braiins-os-2018-09-22-1-hash";
+
+            if args.contains_key("min_version") {
+                min_version = args.get("min_version").unwrap().parse::<u16>().unwrap();
+            }
+
+            if args.contains_key("max_version") {
+                max_version = args.get("max_version").unwrap().parse::<u16>().unwrap();
+            }
+
+            if args.contains_key("vendor") {
+                vendor = args.get("vendor").unwrap();
+            }
+
+            if args.contains_key("firmware") {
+                firmware = args.get("firmware").unwrap();
+            }
+
+            let message = $type::new(
+                min_version,
+                max_version,
+                $flags,
+                "0.0.0.0",
+                8545,
+                vendor,
+                "S9i 13.5",
+                firmware,
+                "some-uuid",
+            );
+
+            message
+        }
+
+        #[test]
+        fn setup_connection_invalid_min_version() {
+            let mut args = HashMap::new();
+            args.insert("min_version".into(), 1.to_string());
+
+            let message = default_setup_connection(args);
+            assert!(message.is_err());
+        }
+
+        #[test]
+        fn setup_connection_invalid_max_version() {
+            let mut args = HashMap::new();
+            args.insert("max_version".into(), 0.to_string());
+
+            let message = default_setup_connection(args);
+            assert!(message.is_err());
+        }
+
+        #[test]
+        fn setup_connection_empty_vendor() {
+            let mut args = HashMap::new();
+            args.insert("vendor".into(), "".to_string());
+
+            let message = default_setup_connection(args);
+
+            assert!(message.is_err())
+        }
+
+        #[test]
+        fn setup_connection_empty_firmware() {
+            let mut args = HashMap::new();
+            args.insert("firmware".into(), "".to_string());
+
+            let message = default_setup_connection(args);
+            assert!(message.is_err())
+        }
+
+        #[test]
+        fn serialize_setup_connection() {
+            let message = default_setup_connection(HashMap::new()).unwrap();
+
+            let buffer = serialize(&message).unwrap();
+            assert_eq!(buffer.len(), 75);
+
+            // Check the protocol enum was serialized correctly.
+            assert_eq!(Protocol::try_from(buffer[0]).unwrap(), message.protocol);
+
+            // Check the flags were serialized correctly.
+            assert_eq!(buffer[5..9], serialize(&message.flags).unwrap());
+
+            // Sanity check - deserializing back to the struct does not cause
+            // errors.
+            assert!(deserialize::<$type>(&buffer).is_ok());
+        }
+
+        #[test]
+        fn serialize_empty_flags() {
+            let message = $type::new(
+                2,
+                2,
+                $empty_flag,
+                "0.0.0.0",
+                8545,
+                "Bitmain",
+                "S9i 13.5",
+                "braiins-os-2018-09-22-1-hash",
+                "some-uuid",
+            )
+            .unwrap();
+
+            let buffer = serialize(&message).unwrap();
+            assert_eq!(buffer.len(), 75);
+
+            // Check that optional flags were serialized correctly when empty.
+            assert_eq!(buffer[5..9], [0u8; 4]);
+        }
+
+        #[test]
+        fn frame_setup_connection() {
+            let message = default_setup_connection(HashMap::new()).unwrap();
+
+            let network_message = frame(&message).unwrap();
+            let buffer = serialize(&network_message).unwrap();
+            assert_eq!(buffer.len(), 81);
+
+            // Check the extension type is empty.
+            assert_eq!(buffer[0..2], [0u8; 2]);
+
+            // Check that the correct byte for the message type was used.
+            assert_eq!(buffer[2], network_message.message_type.msg_type());
+
+            // Check that the correct message length was used.
+            assert_eq!(
+                deserialize::<U24>(&buffer[3..6]).unwrap(),
+                network_message.payload.len() as u32
+            );
+        }
+    };
+}
+
+#[cfg(test)]
+mod mining_connection_tests {
+    use crate::mining::SetupConnection;
+    use crate::mining::SetupConnectionFlags;
+
+    impl_setup_connection_tests!(
+        SetupConnection,
+        SetupConnectionFlags::empty(),
+        SetupConnectionFlags::REQUIRES_STANDARD_JOBS
+            | SetupConnectionFlags::REQUIRES_WORK_SELECTION
+    );
+}
+
+#[cfg(test)]
+mod job_negotiation_tests {
+    use crate::job_negotiation::SetupConnection;
+    use crate::job_negotiation::SetupConnectionFlags;
+
+    impl_setup_connection_tests!(
+        SetupConnection,
+        SetupConnectionFlags::empty(),
+        SetupConnectionFlags::REQUIRES_ASYNC_JOB_MINING
+    );
 }
