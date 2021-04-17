c6a3fe463cb491d6fad7f5fef37c48deff965288
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-04-17 10:47:07 +0100

    remove rexport of common modules

--- stratumv2-lib/src/common/mod.rs
+++ stratumv2-lib/src/common/mod.rs
@@ -1,6 +1,6 @@
-pub mod channel_endpoint_changed;
-pub mod setup_connection;
-pub mod setup_connection_error_code;
+mod channel_endpoint_changed;
+mod setup_connection;
+mod setup_connection_error_code;
 
 pub use channel_endpoint_changed::ChannelEndpointChanged;
 pub use setup_connection::{Protocol, SetupConnection};
