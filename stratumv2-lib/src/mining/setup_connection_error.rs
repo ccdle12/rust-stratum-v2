381e35e03742827d7d07da33539f73bb37916780
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-04-29 11:53:36 +0100

    add further tests for setup_connection_flags and setup_connection_error

--- stratumv2-lib/src/mining/setup_connection_error.rs
+++ stratumv2-lib/src/mining/setup_connection_error.rs
@@ -1,4 +1,34 @@
+use crate::common::SetupConnectionErrorCode;
 use crate::impl_setup_connection_error;
 use crate::mining::SetupConnectionFlags;
 
 impl_setup_connection_error!(SetupConnectionFlags);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::frame::{frame, unframe, Message};
+    use crate::parse::{deserialize, serialize};
+
+    #[test]
+    fn frame_connection_error() {
+        let message = SetupConnectionError::new(
+            SetupConnectionFlags::REQUIRES_STANDARD_JOBS,
+            SetupConnectionErrorCode::UnsupportedFeatureFlags,
+        )
+        .unwrap();
+
+        let network_message = frame(&message).unwrap();
+        let serialized = serialize(&network_message).unwrap();
+
+        // Check the extension type is empty.
+        assert_eq!(serialized[0..=1], [0u8; 2]);
+
+        // Check the msg type is correct.
+        assert_eq!(serialized[2], MessageType::SetupConnectionError.msg_type());
+
+        let der_network_message = deserialize::<Message>(&serialized).unwrap();
+        let deserialized = unframe::<SetupConnectionError>(&der_network_message);
+        assert!(deserialized.is_ok());
+    }
+}
