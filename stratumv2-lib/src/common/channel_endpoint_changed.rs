b9a50091749b17ddc7a363e5057c98759a3bab91
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-05-20 12:40:55 +0100

    [stratumv2-lib]: apply impl_message macro to ChannelEndpointChanged and add tests

--- stratumv2-lib/src/common/channel_endpoint_changed.rs
+++ stratumv2-lib/src/common/channel_endpoint_changed.rs
@@ -1,48 +1,73 @@
 use crate::{
     error::Result,
     frame::Frameable,
+    impl_message,
     parse::{ByteParser, Deserializable, Serializable},
     types::{new_channel_id, MessageType},
 };
 use std::io;
 
-/// When a channel’s upstream or downstream endpoint changes and that channel had previously sent
-/// messages with channel_msg bitset of unknown extension_type, the intermediate proxy MUST send a
-/// ChannelEndpointChanged message. Upon receipt thereof, any extension state (including version
-/// negotiation and the presence of support for a given extension) MUST be reset and
-/// version/presence negotiation must begin again.
-#[derive(Debug, Clone)]
-pub struct ChannelEndpointChanged {
+impl_message!(
+    /// When a channel’s upstream or downstream endpoint changes and that channel had previously sent
+    /// messages with channel_msg bitset of unknown extension_type, the intermediate proxy MUST send a
+    /// ChannelEndpointChanged message. Upon receipt thereof, any extension state (including version
+    /// negotiation and the presence of support for a given extension) MUST be reset and
+    /// version/presence negotiation must begin again.
+    ChannelEndpointChanged,
+    MessageType::ChannelEndpointChanged,
+
     /// The channel which has changed endpoint.
-    channel_id: u32,
-}
+    pub channel_id u32
+);
 
 impl ChannelEndpointChanged {
-    pub fn new() -> ChannelEndpointChanged {
+    pub fn new(channel_id: u32) -> Result<ChannelEndpointChanged> {
+        Ok(ChannelEndpointChanged { channel_id })
+    }
+}
+
+impl Default for ChannelEndpointChanged {
+    fn default() -> Self {
         ChannelEndpointChanged {
             channel_id: new_channel_id(),
         }
     }
 }
 
-impl Serializable for ChannelEndpointChanged {
-    fn serialize<W: io::Write>(&self, writer: &mut W) -> Result<usize> {
-        Ok(self.channel_id.serialize(writer)?)
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::frame::{frame, unframe, Message};
+    use crate::parse::{deserialize, serialize};
+
+    #[test]
+    fn init_channel_endpoint_changed() {
+        let channel_endpoint_changed = ChannelEndpointChanged::default();
+
+        let serialized = serialize(&channel_endpoint_changed).unwrap();
+        assert!(deserialize::<ChannelEndpointChanged>(&serialized).is_ok());
     }
-}
 
-impl Deserializable for ChannelEndpointChanged {
-    fn deserialize(parser: &mut ByteParser) -> Result<ChannelEndpointChanged> {
-        let channel_id = u32::deserialize(parser)?;
+    #[test]
+    fn frame_open_extended_mining() {
+        let network_message = frame(&ChannelEndpointChanged::default()).unwrap();
+        assert_eq!(
+            network_message.message_type,
+            MessageType::ChannelEndpointChanged
+        );
 
-        Ok(ChannelEndpointChanged {
-            channel_id: channel_id,
-        })
-    }
-}
+        let result = serialize(&network_message).unwrap();
 
-impl Frameable for ChannelEndpointChanged {
-    fn message_type() -> MessageType {
-        MessageType::ChannelEndpointChanged
+        // Check the extension type is NOT empty and the MSB is set.
+        // NOTE: the MSB is serialized according to U16 so in serialized form
+        // it will appear as the second byte.
+        assert_eq!(result[0..=1], [0, 128]);
+
+        // Check that the correct byte for the message type was used.
+        assert_eq!(result[2], network_message.message_type.msg_type());
+
+        // Check the network message can be unframed.
+        let deserialized = deserialize::<Message>(&result).unwrap();
+        assert!(unframe::<ChannelEndpointChanged>(&deserialized).is_ok());
     }
 }
