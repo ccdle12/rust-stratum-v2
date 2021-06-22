b1acc7680b0fd45544fd43fba85c536dc0772fb7
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-06-22 15:59:04 +0100

    [stratumv2]: add a shared config struct to network

--- stratumv2/src/network/mod.rs
+++ stratumv2/src/network/mod.rs
@@ -1,7 +1,9 @@
 mod channel_encryptor;
+mod config;
 mod message_handler;
 
 pub use channel_encryptor::ChannelEncryptor;
+pub use config::Config;
 pub use message_handler::{
     JobNegotiationInitiator, MessageHandler, MiningInitiator, NewConnReceiver,
 };
