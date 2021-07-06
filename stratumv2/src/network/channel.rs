156060f61688102bced4cb1ad9f382e815d8ca55
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-07-06 10:28:17 +0100

    [stratumv2]: add Channel struct to network

--- stratumv2/src/network/channel.rs
+++ stratumv2/src/network/channel.rs
@@ -10,6 +10,14 @@ pub fn new_channel_id() -> ChanID {
     rand::thread_rng().gen_range(0, ChanID::MAX)
 }
 
+/// Represents a Channel opened on a connection. The Channel holds stateful
+/// information about the Channel for both Upstream and Downstream devices.
+pub struct Channel {
+    /// The unique ID assigned to a channel for both Upstream and Downstream
+    /// devices.
+    pub chan_id: ChanID,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- stratumv2/src/network/mod.rs
+++ stratumv2/src/network/mod.rs
@@ -4,7 +4,7 @@ mod config;
 mod message_handler;
 mod message_send_event;
 
-pub use channel::{new_channel_id, ChanID};
+pub use channel::{new_channel_id, ChanID, Channel};
 pub use channel_encryptor::ChannelEncryptor;
 pub use config::Config;
 pub use message_handler::{JobNegotiationInitiator, MiningInitiator, NewConnReceiver};
