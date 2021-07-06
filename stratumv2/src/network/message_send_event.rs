2737069c55008de87c9da5657b432bf783cfbfb8
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-07-06 22:00:44 +0100

    [stratumv2]: Remove MessageSendEvent
    Instead of a MessageSendEvent on an outgoing buffer, we can simply use
    the Message struct.

--- stratumv2/src/network/message_send_event.rs
+++
@@ -1,8 +0,0 @@
-use crate::common::SetupConnection;
-
-/// An enum that contains variants for each message that can be sent out on the
-/// wire. This enum is primarily used to cache outgoing messages on a buffer
-/// on each device.
-pub enum MessageSendEvent {
-    SetupConnectionEvent { msg: SetupConnection },
-}
