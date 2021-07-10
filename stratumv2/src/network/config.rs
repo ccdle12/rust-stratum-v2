d53954fadbd4fada7307edd9e3ae76f38f2a1628
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-07-10 11:15:37 +0100

    [stratumv2]: add SignatureNoiseMessage and StaticKeyPair to Config

--- stratumv2/src/network/config.rs
+++ stratumv2/src/network/config.rs
@@ -1,3 +1,5 @@
+use crate::noise::{SignatureNoiseMessage, StaticKeyPair};
+
 /// Config contains the configuration for a networked device. This maybe
 /// separated into Upstream or Downstream configs depending on how each device
 /// requirements begin to diverge. Equally this maybe later moved into an upstream
@@ -6,13 +8,22 @@
 pub struct Config {
     pub listening_addr: String,
     pub local_network_encryption: bool,
+    pub sig_noise_msg: SignatureNoiseMessage,
+    pub static_key: StaticKeyPair,
 }
 
 impl Config {
-    pub fn new(listening_addr: String, local_network_encryption: bool) -> Self {
+    pub fn new(
+        listening_addr: String,
+        local_network_encryption: bool,
+        sig_noise_msg: SignatureNoiseMessage,
+        static_key: StaticKeyPair,
+    ) -> Self {
         Config {
             listening_addr,
             local_network_encryption,
+            sig_noise_msg,
+            static_key,
         }
     }
 }
