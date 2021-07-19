d5aff84000379f8a1f76ae66c41f87beb02465d0
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-07-19 15:20:13 +0100

    TODO: Add different configs

--- stratumv2/src/network/mod.rs
+++ stratumv2/src/network/mod.rs
@@ -5,7 +5,7 @@ mod message_handler;
 mod peer;
 
 pub use channel::{new_channel_id, ChanID, Channel};
-pub use config::Config;
+pub use config::{NetworkConfig, NoiseConfig, ServerConfig};
 pub use encryptor::{ConnectionEncryptor, Encryptor};
 pub use message_handler::{JobNegotiationInitiator, MiningInitiator, NewConnReceiver};
 pub use peer::Peer;
