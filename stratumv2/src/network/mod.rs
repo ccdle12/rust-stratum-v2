1778d2ccdc3c08aef8d3f58f4213d9312edee217
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-07-29 16:30:23 +0100

    [stratumv2]: redesign message handler traits to a per device trait

--- stratumv2/src/network/mod.rs
+++ stratumv2/src/network/mod.rs
@@ -7,5 +7,5 @@ mod peer;
 pub use channel::{new_channel_id, ChanID, Channel, ChannelManager};
 pub use config::{NetworkConfig, NoiseConfig, ServerConfig};
 pub use encryptor::{ConnectionEncryptor, Encryptor};
-pub use message_handler::{JobNegotiationInitiator, MiningInitiator, NewConnReceiver};
+pub use message_handler::{JobNegotiationInitiator, MiningInitiator, ServerMsgHandler};
 pub use peer::Peer;
